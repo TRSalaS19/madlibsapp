faf8921c3839bea469600180882db674f4327115
Gustavo Salas <gustavosalas@Gustavos-MacBook-Pro.local>
2021-03-05 10:22:26 -0800

    added header and header styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,14 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Madlibs Game</h1>
+        <div className="backgroundSkew">
+          <div className="madlib-heading">
+            <h1>Bottega Madlibs Game</h1>
+            <div className="madlib-subheading">
+                Fill out the fields below and click the generate button<br/>to see the Mad Lib story.
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
