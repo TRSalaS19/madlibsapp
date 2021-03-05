ae31adf2a52d74cc2327fc957651b66c07d99b0f
Gustavo Salas <gustavosalas@Gustavos-MacBook-Pro.local>
2021-03-05 11:00:42 -0800

    created madlib form component

---
+++ src/components/madlib_form.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MadlibForm extends React.Component {
+    render() {
+        return (
+      <h1>Bottega Mad Libs Form Component</h1>
+        )
+    }
+  }
+
+export default MadlibForm;
